1997fc355c299c503fb39da650abfd7c7fa1d2a8
omarSemgey <omar.semgey@gmail.com>
2026-09-19 20:57:49 +0300

    fixing static page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <>
-    <Hero />
-    <Skills />
-    <Projects />
-    <Contact />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
     </>
   );
 }
